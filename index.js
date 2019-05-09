4f181253027f8be85bd6e5f0fb927abc3a3ad08b
Sim Greenbaum <gbs4ever2@gmail.com>
2019-05-09 23:30:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 function getToken() {
+  //return '50dcdfc003cf7ff8543f817ded4bdaf7c185134b';
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+return '';
 }
+
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `token ${getToken()}`
+      },
+    method: 'POST'
+  })
+    .then(res => res.json())
+  .then(json => showResults(json))
 }
 
+
 function showResults(json) {
+document.getElementById('results').innerHTML = `<a href ="${json.html_url}">${json.html_url}</a>`
   //use this function to display the results from forking via the API
 }
 
